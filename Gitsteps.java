eb2b46166adbc8fbe303ee7850dded352ef459b6
ottacc66@gmail.com <ottachris13@yahoo.com>
2015-09-21 11:07:48 +0300

    learning git

---
+++ Gitsteps.java
@@ -0,0 +1,11 @@
+public class Gitsteps
+{
+ public static void main(String args)
+ {
+  System.out.println("Learn git step-by-step.");
+  System.out.println("Just added another line of code.");
+ }
+}
+
+
+
